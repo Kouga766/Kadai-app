cd94b597a5ed0f4155a221113ba112d43cc836e5
kouga766 <houkenkokushi@gmail.com>
2021-08-29 16:55:57 +0000

    マイグレート2

--- Bookers/db/schema.rb
+++ Bookers/db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_140822) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
